7eb5f72493862a2cdae66b831a4b667a08146d87
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-04-13 10:01:24 +0700

    thang

--- pages/index/home.js
+++ pages/index/home.js
@@ -1,11 +1,163 @@
-let currentUser = JSON.parse(localStorage.getItem("currentUser")) ;
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 $(document).ready(function () {
     if (currentUser == null) {
         location.href = '/module4-casestudy-frontend/pages/auth/login.html';
     }
 });
+
 function logOut() {
     window.localStorage.clear();
     location.href = '/module4-casestudy-frontend/pages/auth/login.html';
 }
+
+function createNewPost() {
+    let content = $(`#content-post`).val();
+    let status = $(`#status`).val();
+    let image = $(`#image`);
+    let postUserForm = new FormData();
+    postUserForm.append("content", content);
+    postUserForm.append("status", status);
+    postUserForm.append("image", image.prop('files'));
+    $.ajax({
+        type: 'POST',
+        url: `http://localhost:8080/createPost/${current.id}`,
+        data: postUserForm,
+        enctype: "multipart/form-data",
+        processData: false,
+        contentType: false,
+        success: function () {
+            showSuccessMessage('Đăng bài thành công!')
+        },
+        error: function () {
+            showErrorMessage('Đăng bài không thành công!')
+        }
+    })
+}
+
+function displayPost() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/posts',
+        success: function (data) {
+            let content = '';
+            for (let i = 0; i < data.length; i++) {
+
+                content += `<div class="friend-info">
+                                                <figure>
+                                                    <img src="images/resources/friend-avatar10.jpg" alt="">
+                                                </figure>
+                                                <div class="friend-name">
+                                                    <ins><a href="time-line.html" title="">${data[i].userInfo.fullName}</a></ins>
+                                                    <span>${data[i].dateCreated}</span>
+                                                </div>
+                                           <li><a href="#" title="Languages" data-ripple=""><i class="fa fa-globe"></i></a>
+<div class="dropdowns languages">
+<a href="#" title=""><i class="ti-check"></i>English</a>
+<a href="#" title="">Tiếng việt</a>
+
+</div>
+</li>
+                                                 <div class="description">
+
+                                                        <p id="description">
+                                                                ${data[i].content}
+                                                        </p>
+                                                    </div>
+                                                <div class="post-meta">
+                                                <div class="grid-image">`;
+                for (let image of data[i].listImage) {
+                    content += `<img src="http://localhost:8080/image/${image}" alt="">`
+                }
+
+
+                content += `</div><div class="we-video-info">
+                                                        <ul>
+                                                            <li>
+                                                <span class="views" data-toggle="tooltip" title="views">
+                                                <i class="fa fa-eye"></i>
+                                                 <ins>1.2k</ins>
+                                                    </span>
+                                                            </li>
+                                                            <li>
+                                                <span class="comment" data-toggle="tooltip"
+                                                                  title="Comments">
+                                                    <i class="fa fa-comments-o"></i>
+                                                <ins>52</ins>
+                                                </span>
+                                                            </li>
+                                                            <li>
+                                                    <span class="like" data-toggle="tooltip" title="like">
+                                                    <i class="ti-heart"></i>
+                                                    <ins>2.2k</ins>
+                                                </span>
+                                                            </li>
+                                                            <li>
+                                                <span class="dislike" data-toggle="tooltip" title="dislike">
+                                            <i class="ti-heart-broken"></i>
+                                        <ins>200</ins>
+                                            </span>
+                                                            </li>
+                                                            <li class="social-media">
+                                                                <div class="menu">
+                                                                    <div class="btn trigger"><i
+                                                                            class="fa fa-share-alt"></i></div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-html5"></i></a></div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-facebook"></i></a></div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-google-plus"></i></a></div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-twitter"></i></a></div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-css3"></i></a></div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-instagram"></i></a>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-dribbble"></i></a>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="rotater">
+                                                                        <div class="btn btn-icon"><a href="#"
+                                                                                                     title=""><i
+                                                                                class="fa fa-pinterest"></i></a>
+                                                                        </div>
+                                                                    </div>
+
+                                                                </div>
+                                                            </li>
+                                                        </ul>   
+                                                    </div>
+                                                  
+                                                </div>
+                                            </div>`
+            }
+            $(`#post_User`).html(content);
+        }
+
+    })
+}
+
+displayPost();
